b298a46b77f1c8bf8fc721850bcf1c2926011135
Josh Marasigan <marasigan.josh@utexas.edu>
2017-04-29 18:07:43 -0500

    search page

--- src/js/components/Global/Banner.jsx
+++ src/js/components/Global/Banner.jsx
@@ -4,7 +4,7 @@ export default class Banner extends Component {
   render() {
     return (
       <div className="banner">
-        <img height="42" width="42" src="http://tinyimg.io/i/vRreWOs.png" />
+        <img src="http://tinyimg.io/i/btWKblI.png" />
         <h1>MEDREC</h1>
       </div>
     );
